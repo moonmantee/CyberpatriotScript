63a3ae5010fda5148b405c72b4b476b07771c386
moonmantee <jwan56w@gmail.com>
2024-10-25 10:09:05 -0600

    modified the localSecurity.ps1 to download then import policy

--- localSecurity.ps1
+++ localSecurity.ps1
@@ -1,2 +1,2 @@
-Start-Process -FilePath "rundll32" -ArgumentList "setupapi,InstallHinfSection DefaultInstall 132 (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/moonmantee/CyberpatriotScript/Server2022SecPolTemplate.inf' -OutFile 'C:\template.inf'); 'C:\template.inf'" -Verb RunAs
-
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/moonmantee/CyberpatriotScript/main/Server2022SecPolTemplate.inf' -OutFile 'C:\template.inf'
+secedit /import /db C:\Windows\security\database\secedit.sdb /cfg C:\template.inf
