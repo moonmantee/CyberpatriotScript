e1452c69ffe568f3efcd17e1e82cafcf7ba8b265
moonmantee <jwan56w@gmail.com>
2024-11-20 09:03:17 -0700

    Updated path

--- Windows/securityPolicy.ps1
+++ Windows/securityPolicy.ps1
@@ -1,5 +1,5 @@
 # Download security template, and import the local template
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/moonmantee/CyberpatriotScript/main/Server2022SecPolTemplate.inf' -OutFile 'C:\template.inf'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/moonmantee/CyberpatriotScript/main/Windows/Server2022SecPolTemplate.inf' -OutFile 'C:\template.inf'
 secedit /import /db C:\Windows\security\database\secedit.sdb /cfg C:\template.inf
 
 
